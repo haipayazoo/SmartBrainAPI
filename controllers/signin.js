b079e2b1117303ef7d11fa8a64b72f5a11001e0d
haipayazoo <haipayazoo@gmail.com>
2020-04-08 16:53:17 -0700

    Sending user a JWT Token on successful login

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const handleSignin = (db, bcrypt, req, res) => {
 	const { email, password } = req.body;
 
@@ -29,12 +31,28 @@ const getAuthTokenId = () => {
 	console.log('auth ok');
 };
 
+const signToken = (email) => {
+	const jwtPayload = { email };
+	return jwt.sign(jwtPayload, 'JWT-SECRET', { expiresIn: '2 days' });
+};
+
+const createSessions = (user) => {
+	const { email, id } = user;
+	const token = signToken(email);
+	return { success: 'true', userId: id, token };
+};
+
 const signinAuthentication = (db, bcrypt) => (req, res) => {
 	const { authorization } = req.headers;
 	return authorization
 		? getAuthTokenId()
 		: handleSignin(db, bcrypt, req, res)
-				.then((data) => res.json(data))
+				.then((data) => {
+					return data.id && data.email
+						? createSessions(data)
+						: Promise.reject(data);
+				})
+				.then((session) => res.json(session))
 				.catch((err) => res.status(400).json(err));
 };
 
